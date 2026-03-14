a64c711cd7f3abae7f26881969457658e0cf8c41
matthewcflam <matthewcflam@gmail.com>
2026-03-13 18:28:44 -0700

    setGrey and setLOB

--- pa2-20260214-1452/treasuremap.cpp
+++ pa2-20260214-1452/treasuremap.cpp
@@ -9,18 +9,41 @@
 using namespace std;
 
 TreasureMap::TreasureMap(const PNG& baseim, const PNG& mazeim, pair<int, int> s) {
-	/* YOUR CODE HERE */
-
+	start = s;
+	base = baseim;
+	maze = mazeim;
 }
 
+/**
+     * Changes the pixel at position loc to consist of colour channel
+     * values which are 50% of their original. Note that because of 
+     * our encoding scheme which essentially invalidates the lower
+     * order bits, you should compute each value as 2*(x/4), where
+     * x is the integer value of a colour channel.
+    **/
 void TreasureMap::SetGrey(PNG& im, pair<int, int> loc) {
-	/* YOUR CODE HERE */
+	int x = loc.first;
+	int y = loc.second;
 
+	RGBAPixel *pixel = im.getPixel(x, y);
+	
+	pixel->r = 2 * (pixel->r / 4);
+    pixel->g = 2 * (pixel->g / 4);
+    pixel->b = 2 * (pixel->b / 4);
 }
 
 void TreasureMap::SetLOB(PNG& im, pair<int, int> loc, int d) {
-	/* YOUR CODE HERE */
+    RGBAPixel *pixel = im.getPixel(loc.first, loc.second);
 
+    int d6bit = d % 64;
+
+    unsigned char rbits = (d6bit >> 4) & 0x3; 
+    unsigned char gbits = (d6bit >> 2) & 0x3; 
+    unsigned char bbits = d6bit & 0x3;        
+
+    pixel->r = (pixel->r & 0xFC) | rbits;
+    pixel->g = (pixel->g & 0xFC) | gbits;
+    pixel->b = (pixel->b & 0xFC) | bbits;
 }
 
 PNG TreasureMap::RenderMap() {
